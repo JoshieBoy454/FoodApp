2fb8d4e7329c6621f77f89615d17b46b86a53f61
Joshua Gain <ST10369044@vcconnect.edu.za>
2024-04-15 20:39:03 +0200

    Added array and variables for Ingredients and recipe steps

---
+++ FoodAppWorker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodApp
+{
+    public class FoodAppWorker
+    {
+        ArrayList ingredientsArray = new ArrayList();
+        ArrayList stepsArray = new ArrayList();
+
+        public String steps { get; set; }
+        public String ingredients { get; set; }
+
+        public void recipeDetails()
+        {
+        }   
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,21 @@ namespace FoodApp
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Welcome to the Food App!");
+            Console.WriteLine("Please enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+            Console.WriteLine("Please enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You are " + age + " years old.");
+            Console.WriteLine("Please enter your favorite food: ");
+            string food = Console.ReadLine();
+            Console.WriteLine("Your favorite food is " + food + ".");
+            Console.WriteLine("Please enter your favorite drink: ");
+            string drink = Console.ReadLine();
+            Console.WriteLine("Your favorite drink is " + drink + ".");
+            Console.WriteLine("Thank you for using the Food App!");
+            Console.ReadLine();
         }
     }
 }
