e545941da49f7d8f26614b33881d30af50c260ad
Joshua Gain <ST10369044@vcconnect.edu.za>
2024-04-16 22:30:40 +0200

    Eddited resetRecipe() which now ask the user to confirm and added try catch for error handeling

--- FoodAppWorker.cs
+++ FoodAppWorker.cs
@@ -15,7 +15,7 @@ namespace FoodApp
     {
         ArrayList recipeArray = new ArrayList();
         recipe newRecipe = new recipe();
-        int scale;
+        double scale;
 
 //----------------------------------------------------------------------------------------->
         //ingredient class contains the name, quantity and measurement of the ingredient
@@ -104,13 +104,21 @@ namespace FoodApp
         //scales the recipe ingredients by a user inputted amount
         public void recipeScale(recipe newRecipe)
         {
-            Console.WriteLine("Enter the amount you'd like to scale your recipe by: ");
-            scale = Convert.ToInt32(Console.ReadLine());
-            foreach (ingredient ingredient in newRecipe.ingredient)
+            try
             {
-                ingredient.quantity = (Convert.ToInt32(ingredient.quantity) * scale).ToString();
+                Console.WriteLine("Enter the amount you'd like to scale your recipe by: ");
+                scale = Convert.ToDouble(Console.ReadLine());
+                foreach (ingredient ingredient in newRecipe.ingredient)
+                {
+                    ingredient.quantity = (Convert.ToInt32(ingredient.quantity) * scale).ToString();
+                }
+                Menu();
             }
-            Menu();
+            catch (Exception e)
+            {
+                Console.WriteLine("Invalid input please enter a number");
+                Menu();
+            }
         }
 //----------------------------------------------------------------------------------------->
         //resets the recipe scale to the original amount
@@ -123,55 +131,72 @@ namespace FoodApp
             Menu();
         }
 //----------------------------------------------------------------------------------------->
-        //resets the recipe to the original details
+        //resets the recipe to the original details - asks the user to confirm
         public void resetRecipe(recipe newRecipe)
         {
-            foreach(recipe r in recipeArray)
+            Console.WriteLine("Are you sure you would like to reset your recipe?");
+            Console.WriteLine("1. Yes");
+            Console.WriteLine("2. No");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if (choice == 1)
             {
-                r.name = null;
-                r.ingredient.Clear();
-                r.step.Clear();
+                newRecipe.name = null;
+                newRecipe.ingredient.Clear();
+                newRecipe.step.Clear();
+                Console.WriteLine("Recipe reset");
             }
-            Console.WriteLine("Recipe reset");
+            else if (choice == 2)
+            {
+                Console.WriteLine("Recipe not reset");
+            }
             Menu();
         }
 //----------------------------------------------------------------------------------------->
         //menu for the user to choose what they want to do
         public void Menu()
         {
-            Console.WriteLine("-----------------------------------");
-            Console.WriteLine("1. Add a recipe");
-            Console.WriteLine("2. Print a recipe");
-            Console.WriteLine("3. Scale recipe ingredients");
-            Console.WriteLine("4. Reset recipe scale");
-            Console.WriteLine("5. Reset recipe");
-            Console.WriteLine("6. Exit");
-            Console.WriteLine("-----------------------------------");
-            Console.WriteLine("Enter your choice: ");
-            int choice = Convert.ToInt32(Console.ReadLine());
-            switch (choice)
+            try
             {
-                case 1:
-                    recipeInputDetails(newRecipe);
-                    break;
-                case 2:
-                    recipePrint(newRecipe);
-                    break;
-                case 3:
-                    recipeScale(newRecipe);
-                    break;
-                case 4:
-                    resetScale(newRecipe);
-                    break;
-                case 5:
-                    resetRecipe(newRecipe);
-                    break;
-                case 6:
-                    Environment.Exit(0);
-                    break;
-                default:
-                    Console.WriteLine("Invalid choice");
-                    break;
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("1. Add a recipe");
+                Console.WriteLine("2. Print a recipe");
+                Console.WriteLine("3. Scale recipe ingredients");
+                Console.WriteLine("4. Reset recipe scale");
+                Console.WriteLine("5. Reset recipe");
+                Console.WriteLine("6. Exit");
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("Enter your choice: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        recipeInputDetails(newRecipe);
+                        break;
+                    case 2:
+                        recipePrint(newRecipe);
+                        break;
+                    case 3:
+                        recipeScale(newRecipe);
+                        break;
+                    case 4:
+                        resetScale(newRecipe);
+                        break;
+                    case 5:
+                        resetRecipe(newRecipe);
+                        break;
+                    case 6:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice please enter a number from 1-6");
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Invalid input please enter a number");
+                Menu();
             }
         }
 //----------------------------------------------------------------------------------------->
